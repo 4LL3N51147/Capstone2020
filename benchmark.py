2e980651df86162fe1b52aa5ee01763b897e1d80
root <woshiallenyang@hotmail.com>
2020-10-01 23:48:30 +0000

    benchmark 2.0

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,15 @@
-from subprocess import run
+import subprocess
+import os
 
-bashCommand = "python3 fawkes/protection.py -d {} -m {} > {}".format(dir, mode, logFilePath)
-
-run(bashCommand)
+parent_directory = 'imgs'
+#modes = ['min', "low", "mid", "high"]
+modes = ['min']
+for folder in os.scandir(parent_directory):
+    if folder.is_dir():
+        # print(folder.name)
+        for mode in modes:
+            print("Currently running Fawkes with {} in {} mode".format(folder.name, mode))
+            logFilePath = os.path.join(os.path.expanduser('~'), folder.name+'_'+ mode+'.log')
+            bashCommand = "python3 ./fawkes/protection.py -d {} -m {} > {}".format(folder.name, mode, logFilePath)
+            process = subprocess.run(bashCommand, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+            print("Log file saved to {}".format(logFilePath))
