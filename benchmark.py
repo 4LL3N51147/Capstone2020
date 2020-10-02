89c17589cf5a77a150542c11ca9fd2b0f2589a0d
root <woshiallenyang@hotmail.com>
2020-10-02 00:53:06 +0000

    benchmark 2.1

--- benchmark.py
+++ benchmark.py
@@ -10,6 +10,6 @@ for folder in os.scandir(parent_directory):
         for mode in modes:
             print("Currently running Fawkes with {} in {} mode".format(folder.name, mode))
             logFilePath = os.path.join(os.path.expanduser('~'), folder.name+'_'+ mode+'.log')
-            bashCommand = "python3 ./fawkes/protection.py -d {} -m {} > {}".format(folder.name, mode, logFilePath)
+            bashCommand = "python3 ./fawkes/protection.py -d {} -m {} > {}".format(os.path.join(parent_directory, folder.name), mode, logFilePath)
             process = subprocess.run(bashCommand, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
             print("Log file saved to {}".format(logFilePath))
