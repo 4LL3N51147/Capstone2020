134215cac9501c1d9a571b1d7527671d531321f0
unknown <woshiallenyang@hotmail.com>
2020-10-08 12:51:03 -0400

    added CPU config in utils

--- fawkes/protection.py
+++ fawkes/protection.py
@@ -17,229 +17,229 @@ logging.getLogger('tensorflow').disabled = True
 import numpy as np
 from fawkes.differentiator import FawkesMaskGeneration
 from fawkes.utils import load_extractor, init_gpu, select_target_label, dump_image, reverse_process_cloaked, \
-    Faces, filter_image_paths
+	Faces, filter_image_paths
 
 from fawkes.align_face import aligner
 from fawkes.utils import get_file
 
 def generate_cloak_images(protector, image_X, target_emb=None):
-    cloaked_image_X = protector.attack(image_X, target_emb)
-    return cloaked_image_X
+	cloaked_image_X = protector.attack(image_X, target_emb)
+	return cloaked_image_X
 
 
 class Fawkes(object):
-    def __init__(self, feature_extractor, gpu, batch_size):
-        self.feature_extractor = feature_extractor
-        self.gpu = gpu
-        self.batch_size = batch_size
-        global sess
-        sess = init_gpu(gpu)
-        global graph
-        graph = tf.get_default_graph()
+	def __init__(self, feature_extractor, gpu, batch_size):
+		self.feature_extractor = feature_extractor
+		self.gpu = gpu
+		self.batch_size = batch_size
+		global sess
+		sess = init_gpu(gpu)
+		global graph
+		graph = tf.get_default_graph()
 
-        model_dir = os.path.join(os.path.expanduser('~'), '.fawkes')
-        if not os.path.exists(os.path.join(model_dir, "mtcnn.p.gz")):
-            os.makedirs(model_dir, exist_ok=True)
-            get_file("mtcnn.p.gz", "http://mirror.cs.uchicago.edu/fawkes/files/mtcnn.p.gz", cache_dir=model_dir,
-                     cache_subdir='')
+		model_dir = os.path.join(os.path.expanduser('~'), '.fawkes')
+		if not os.path.exists(os.path.join(model_dir, "mtcnn.p.gz")):
+			os.makedirs(model_dir, exist_ok=True)
+			get_file("mtcnn.p.gz", "http://mirror.cs.uchicago.edu/fawkes/files/mtcnn.p.gz", cache_dir=model_dir,
+					 cache_subdir='')
 
-        self.fs_names = [feature_extractor]
-        if isinstance(feature_extractor, list):
-            self.fs_names = feature_extractor
+		self.fs_names = [feature_extractor]
+		if isinstance(feature_extractor, list):
+			self.fs_names = feature_extractor
 
-        self.aligner = aligner(sess)
-        self.feature_extractors_ls = [load_extractor(name) for name in self.fs_names]
+		self.aligner = aligner(sess)
+		self.feature_extractors_ls = [load_extractor(name) for name in self.fs_names]
 
-        self.protector = None
-        self.protector_param = None
+		self.protector = None
+		self.protector_param = None
 
-    # assign values based on the mode selected
-    def mode2param(self, mode):
-        if mode == 'min':
-            th = 0.002
-            max_step = 20
-            lr = 40
-        elif mode == 'low':
-            th = 0.003
-            max_step = 50
-            lr = 35
-        elif mode == 'mid':
-            th = 0.005
-            max_step = 200
-            lr = 20
-        elif mode == 'high':
-            th = 0.008
-            max_step = 500
-            lr = 10
-        elif mode == 'ultra':
-            if not tf.test.is_gpu_available():
-                print("Please enable GPU for ultra setting...")
-                sys.exit(1)
-            th = 0.01
-            max_step = 1000
-            lr = 8
-        else:
-            raise Exception("mode must be one of 'min', 'low', 'mid', 'high', 'ultra', 'custom'")
-        return th, max_step, lr
+	# assign values based on the mode selected
+	def mode2param(self, mode):
+		if mode == 'min':
+			th = 0.002
+			max_step = 20
+			lr = 40
+		elif mode == 'low':
+			th = 0.003
+			max_step = 50
+			lr = 35
+		elif mode == 'mid':
+			th = 0.005
+			max_step = 200
+			lr = 20
+		elif mode == 'high':
+			th = 0.008
+			max_step = 500
+			lr = 10
+		elif mode == 'ultra':
+			if not tf.test.is_gpu_available():
+				print("Please enable GPU for ultra setting...")
+				sys.exit(1)
+			th = 0.01
+			max_step = 1000
+			lr = 8
+		else:
+			raise Exception("mode must be one of 'min', 'low', 'mid', 'high', 'ultra', 'custom'")
+		return th, max_step, lr
 
-    def run_protection(self, image_paths, mode='min', th=0.04, sd=1e9, lr=10, max_step=500, batch_size=1, format='png',
-                       separate_target=True, debug=True, no_align=False):
-        if mode == 'custom':
-            pass
-        else:
-            th, max_step, lr = self.mode2param(mode)
+	def run_protection(self, image_paths, mode='min', th=0.04, sd=1e9, lr=10, max_step=500, batch_size=1, format='png',
+					   separate_target=True, debug=True, no_align=False):
+		if mode == 'custom':
+			pass
+		else:
+			th, max_step, lr = self.mode2param(mode)
 
-        current_param = "-".join([str(x) for x in [mode, th, sd, lr, max_step, batch_size, format,
-                                                   separate_target, debug]])
-        print("Fininshed Setting Parameters at: {}".format(datetime.now()))
-        image_paths, loaded_images = filter_image_paths(image_paths)
-        print("Finished Loading Images at: {}".format(datetime.now()))
+		current_param = "-".join([str(x) for x in [mode, th, sd, lr, max_step, batch_size, format,
+												   separate_target, debug]])
+		print("Fininshed Setting Parameters at: {}".format(datetime.now()))
+		image_paths, loaded_images = filter_image_paths(image_paths)
+		print("Finished Loading Images at: {}".format(datetime.now()))
 
-        if not image_paths:
-            print("No images in the directory")
-            return 3
+		if not image_paths:
+			print("No images in the directory")
+			return 3
 
-        with graph.as_default():
-            faces = Faces(image_paths, loaded_images, self.aligner, verbose=1, no_align=no_align)
-            original_images = faces.cropped_faces
-            print("Finished Detecting Faces In The Images at: {}".format(datetime.now()))
+		with graph.as_default():
+			faces = Faces(image_paths, loaded_images, self.aligner, verbose=1, no_align=no_align)
+			original_images = faces.cropped_faces
+			print("Finished Detecting Faces In The Images at: {}".format(datetime.now()))
 
-            if len(original_images) == 0:
-                print("No face detected. ")
-                return 2
-            original_images = np.array(original_images)
+			if len(original_images) == 0:
+				print("No face detected. ")
+				return 2
+			original_images = np.array(original_images)
 
-            with sess.as_default():
-                if separate_target:
-                    target_embedding = []
-                    i = 0
-                    print("Start image reshape at: {}".format(datetime.now()))
-                    for org_img in original_images:
-                        org_img = org_img.reshape([1] + list(org_img.shape))
-                        print("Finished Image {} Reshape at: {}".format(i, datetime.now()))
-                        tar_emb = select_target_label(org_img, self.feature_extractors_ls, self.fs_names)
-                        print("Finished Target Embedding Image {} at: {}".format(i, datetime.now()))
-                        target_embedding.append(tar_emb)
-                        i += 1
-                    target_embedding = np.concatenate(target_embedding)
-                else:
-                    target_embedding = select_target_label(original_images, self.feature_extractors_ls, self.fs_names)
-                print("Finished All Images Target Embedding at: {}".format(datetime.now()))
+			with sess.as_default():
+				if separate_target:
+					target_embedding = []
+					i = 0
+					print("Start image reshape at: {}".format(datetime.now()))
+					for org_img in original_images:
+						org_img = org_img.reshape([1] + list(org_img.shape))
+						print("Finished Image {} Reshape at: {}".format(i, datetime.now()))
+						tar_emb = select_target_label(org_img, self.feature_extractors_ls, self.fs_names)
+						print("Finished Target Embedding Image {} at: {}".format(i, datetime.now()))
+						target_embedding.append(tar_emb)
+						i += 1
+					target_embedding = np.concatenate(target_embedding)
+				else:
+					target_embedding = select_target_label(original_images, self.feature_extractors_ls, self.fs_names)
+				print("Finished All Images Target Embedding at: {}".format(datetime.now()))
 
-                if current_param != self.protector_param:
-                    self.protector_param = current_param
+				if current_param != self.protector_param:
+					self.protector_param = current_param
 
-                    if self.protector is not None:
-                        del self.protector
-                
-                    self.protector = FawkesMaskGeneration(sess, self.feature_extractors_ls,
-                                                          batch_size=batch_size,
-                                                          mimic_img=True,
-                                                          intensity_range='imagenet',
-                                                          initial_const=sd,
-                                                          learning_rate=lr,
-                                                          max_iterations=max_step,
-                                                          l_threshold=th,
-                                                          verbose=1 if debug else 0,
-                                                          maximize=False,
-                                                          keep_final=False,
-                                                          image_shape=(224, 224, 3))
-                
-                print("Finished Initializing Fawkes Protector at: {}".format(datetime.now()))
+					if self.protector is not None:
+						del self.protector
+				
+					self.protector = FawkesMaskGeneration(sess, self.feature_extractors_ls,
+														  batch_size=batch_size,
+														  mimic_img=True,
+														  intensity_range='imagenet',
+														  initial_const=sd,
+														  learning_rate=lr,
+														  max_iterations=max_step,
+														  l_threshold=th,
+														  verbose=1 if debug else 0,
+														  maximize=False,
+														  keep_final=False,
+														  image_shape=(224, 224, 3))
+				
+				print("Finished Initializing Fawkes Protector at: {}".format(datetime.now()))
 
-                protected_images = generate_cloak_images(self.protector, original_images,
-                                                         target_emb=target_embedding)
-                
-                print("Finished Protecting Images at: {}".format(datetime.now()))
+				protected_images = generate_cloak_images(self.protector, original_images,
+														 target_emb=target_embedding)
+				
+				print("Finished Protecting Images at: {}".format(datetime.now()))
 
-                faces.cloaked_cropped_faces = protected_images
+				faces.cloaked_cropped_faces = protected_images
 
-                final_images = faces.merge_faces(reverse_process_cloaked(protected_images),
-                                                 reverse_process_cloaked(original_images))
-                print("Finished Merging Faces at: {}".format(datetime.now()))
+				final_images = faces.merge_faces(reverse_process_cloaked(protected_images),
+												 reverse_process_cloaked(original_images))
+				print("Finished Merging Faces at: {}".format(datetime.now()))
 
-        for p_img, path in zip(final_images, image_paths):
-            file_name = "{}_{}_cloaked.{}".format(".".join(path.split(".")[:-1]), mode, format)
-            dump_image(p_img, file_name, format=format)
+		for p_img, path in zip(final_images, image_paths):
+			file_name = "{}_{}_cloaked.{}".format(".".join(path.split(".")[:-1]), mode, format)
+			dump_image(p_img, file_name, format=format)
 
-        print("Done! and Finished Exporting File at: {}".format(datetime.now()))
-        return 1
+		print("Done! and Finished Exporting File at: {}".format(datetime.now()))
+		return 1
 
 
 def main(*argv):
-    if not argv:
-        argv = list(sys.argv)
+	if not argv:
+		argv = list(sys.argv)
 
-    try:
-        import signal
-        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-    except Exception as e:
-        pass
+	try:
+		import signal
+		signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+	except Exception as e:
+		pass
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--directory', '-d', type=str,
-                        help='the directory that contains images to run protection', default='imgs/')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--directory', '-d', type=str,
+						help='the directory that contains images to run protection', default='imgs/')
 
-    parser.add_argument('--gpu', '-g', type=str,
-                        help='the GPU id when using GPU for optimization', default='0')
+	parser.add_argument('--gpu', '-g', type=str,
+						help='the GPU id when using GPU for optimization', default='0')
 
-    parser.add_argument('--mode', '-m', type=str,
-                        help='cloak generation mode, select from min, low, mid, high. The higher the mode is, the more perturbation added and stronger protection',
-                        default='min')
+	parser.add_argument('--mode', '-m', type=str,
+						help='cloak generation mode, select from min, low, mid, high. The higher the mode is, the more perturbation added and stronger protection',
+						default='min')
 
-    parser.add_argument('--feature-extractor', type=str,
-                        help="name of the feature extractor used for optimization, currently only support high_extract",
-                        default="high_extract")
+	parser.add_argument('--feature-extractor', type=str,
+						help="name of the feature extractor used for optimization, currently only support high_extract",
+						default="high_extract")
 
-    parser.add_argument('--th', help='only relevant with mode=custom, DSSIM threshold for perturbation', type=float,
-                        default=0.01)
-    parser.add_argument('--max-step', help='only relevant with mode=custom, number of steps for optimization', type=int,
-                        default=1000)
-    parser.add_argument('--sd', type=int, help='only relevant with mode=custom, penalty number, read more in the paper',
-                        default=1e9)
-    parser.add_argument('--lr', type=float, help='only relevant with mode=custom, learning rate', default=2)
+	parser.add_argument('--th', help='only relevant with mode=custom, DSSIM threshold for perturbation', type=float,
+						default=0.01)
+	parser.add_argument('--max-step', help='only relevant with mode=custom, number of steps for optimization', type=int,
+						default=1000)
+	parser.add_argument('--sd', type=int, help='only relevant with mode=custom, penalty number, read more in the paper',
+						default=1e9)
+	parser.add_argument('--lr', type=float, help='only relevant with mode=custom, learning rate', default=2)
 
-    parser.add_argument('--batch-size', help="number of images to run optimization together", type=int, default=1)
-    parser.add_argument('--separate_target', help="whether select separate targets for each faces in the directory",
-                        action='store_true')
-    parser.add_argument('--no-align', help="whether to detect and crop faces",
-                        action='store_true')
-    parser.add_argument('--debug', help="turn on debug and copy/paste the stdout when reporting an issue on github",
-                        action='store_true')
-    parser.add_argument('--format', type=str,
-                        help="format of the output image",
-                        default="png")
-    
-    print("Finished Fawkes CLI tools argument setup at: {}".format(datetime.now()))
+	parser.add_argument('--batch-size', help="number of images to run optimization together", type=int, default=1)
+	parser.add_argument('--separate_target', help="whether select separate targets for each faces in the directory",
+						action='store_true')
+	parser.add_argument('--no-align', help="whether to detect and crop faces",
+						action='store_true')
+	parser.add_argument('--debug', help="turn on debug and copy/paste the stdout when reporting an issue on github",
+						action='store_true')
+	parser.add_argument('--format', type=str,
+						help="format of the output image",
+						default="png")
+	
+	print("Finished Fawkes CLI tools argument setup at: {}".format(datetime.now()))
 
-    args = parser.parse_args(argv[1:])
+	args = parser.parse_args(argv[1:])
 
-    assert args.format in ['png', 'jpg', 'jpeg']
-    if args.format == 'jpg':
-        args.format = 'jpeg'
+	assert args.format in ['png', 'jpg', 'jpeg']
+	if args.format == 'jpg':
+		args.format = 'jpeg'
 
-    image_paths = glob.glob(os.path.join(args.directory, "*"))
-    image_paths = [path for path in image_paths if "_cloaked" not in path.split("/")[-1]]
+	image_paths = glob.glob(os.path.join(args.directory, "*"))
+	image_paths = [path for path in image_paths if "_cloaked" not in path.split("/")[-1]]
 
-    print("Finished Loading Images Paths at: {}".format(datetime.now()))
+	print("Finished Loading Images Paths at: {}".format(datetime.now()))
 
-    protector = Fawkes(args.feature_extractor, args.gpu, args.batch_size)
-    print("Finished Initializing Fawkes at: {}".format(datetime.now()))
+	protector = Fawkes(args.feature_extractor, args.gpu, args.batch_size)
+	print("Finished Initializing Fawkes at: {}".format(datetime.now()))
 
-    if args.mode == 'all':
-        for mode in ['min', 'low', 'mid', 'high']:
-            print("Start running protection at: {}".format(datetime.now()))
-            protector.run_protection(image_paths, mode=mode, th=args.th, sd=args.sd, lr=args.lr,
-                                     max_step=args.max_step,
-                                     batch_size=args.batch_size, format=args.format,
-                                     separate_target=args.separate_target, debug=args.debug, no_align=args.no_align)
-    else:
-        print("Start running protection at: {}".format(datetime.now()))
-        protector.run_protection(image_paths, mode=args.mode, th=args.th, sd=args.sd, lr=args.lr,
-                                 max_step=args.max_step,
-                                 batch_size=args.batch_size, format=args.format,
-                                 separate_target=args.separate_target, debug=args.debug, no_align=args.no_align)
+	if args.mode == 'all':
+		for mode in ['min', 'low', 'mid', 'high']:
+			print("Start running protection at: {}".format(datetime.now()))
+			protector.run_protection(image_paths, mode=mode, th=args.th, sd=args.sd, lr=args.lr,
+									 max_step=args.max_step,
+									 batch_size=args.batch_size, format=args.format,
+									 separate_target=args.separate_target, debug=args.debug, no_align=args.no_align)
+	else:
+		print("Start running protection at: {}".format(datetime.now()))
+		protector.run_protection(image_paths, mode=args.mode, th=args.th, sd=args.sd, lr=args.lr,
+								 max_step=args.max_step,
+								 batch_size=args.batch_size, format=args.format,
+								 separate_target=args.separate_target, debug=args.debug, no_align=args.no_align)
 
 
 if __name__ == '__main__':
-    main(*sys.argv)
+	main(*sys.argv)
