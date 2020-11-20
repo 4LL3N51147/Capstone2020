b544728e8ee85b5ab96e9d081725ae56037d25b4
unknown <woshiallenyang@hotmail.com>
2020-11-20 01:54:07 -0500

    basic python api implemented

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ import imghdr
 import subprocess
 import os
 import time
-from flask import Flask, render_template, request, redirect, url_for, abort, Response
+import secrets 
+import string
+import pathlib
+from flask import Flask, render_template, request, redirect, url_for, abort, Response, send_file
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -10,52 +13,49 @@ app = Flask(__name__)
 app.config['UPLOAD_EXTENSIONS'] = ['.jpeg', '.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
 
+def genSecretToken(N=16): 
+	res = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(N))
+	return str(res)
+
 def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0) 
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
+	header = stream.read(512)
+	stream.seek(0) 
+	format = imghdr.what(None, header)
+	if not format:
+		return None
+	return '.' + (format if format != 'jpeg' else 'jpg')
 
-def cloak_image(mode):
-    bashCommand = "python ./fawkes/protection.py -d {} -m {}".format("./uploads", mode)
-    process = subprocess.run(bashCommand, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-    print(process)
-
-@app.route('/yield')
-def yieldRoute():
-    def inner():
-        proc = subprocess.Popen(
-            ['dmesg'],             #call something with a lot of output so we can see it
-            shell=True,
-            stdout=subprocess.PIPE
-        )
-
-        for line in iter(proc.stdout.readline,''):
-            time.sleep(1)                           # Don't need this just shows the text streaming
-            yield line.rstrip() + '<br/>\n'
-
-    return Response(inner(), mimetype='text/html')
+def cloak_image(path, mode):
+	bashCommand = ["python", "./fawkes/protection.py", "-d", os.path.join(app.config['UPLOAD_PATH'], path), "-m", str(mode)]
+	process = subprocess.Popen(bashCommand)
+	return process.wait()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    selected_mode = request.form['mode']
-    file_full = secure_filename(uploaded_file.filename)
-    if file_full != '':
-        file_name, file_ext = os.path.splitext(file_full)
-        file_name += "_{}".format(selected_mode)
-        print(file_name, file_ext)
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            return "unsupported file format"
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], file_name+file_ext))
-        cloak_image(selected_mode)
-    return redirect(url_for('index'))
+	token = genSecretToken()
+	print(token)
+	uploaded_file = request.files['file']
+	selected_mode = request.form['mode']
+	file_full = secure_filename(uploaded_file.filename)
+	if file_full != '':
+		file_name, file_ext = os.path.splitext(file_full)
+		if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+				file_ext != validate_image(uploaded_file.stream):
+			return "unsupported file format"
+		pathlib.Path(app.config['UPLOAD_PATH'], token).mkdir(exist_ok=True)
+		uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], token, file_name+file_ext))
+		cloak_image(token, selected_mode)
+	else:
+		return redirect(url_for("index"))
+
+	cloaked_image_path = os.path.join(app.config['UPLOAD_PATH'], token, file_name+"_{}_cloaked.png".format(selected_mode))
+	if os.path.exists(cloaked_image_path):
+		return send_file(cloaked_image_path)
+	else:
+		return cloaked_image_path
 
 app.run()
